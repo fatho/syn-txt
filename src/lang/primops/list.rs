08696ee342cb11e335b26aa8d91d0961aa8e16e3
Fabian Thorand <f.thorand@gmail.com>
2020-05-15 22:49:23 +0200

    Add GNU license header to files where it was missing

--- src/lang/primops/list.rs
+++ src/lang/primops/list.rs
@@ -1,3 +1,13 @@
+// syn.txt -- a text based synthesizer and audio workstation
+// Copyright (C) 2020  Fabian Thorand
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation.
+//
+// A copy of the license can be found in the LICENSE file in the root of
+// this repository.
+
 //! List operations
 
 use crate::lang::heap::*;

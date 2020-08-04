9f8952aec2e6bf8ab6d9e0e1d4eb3e327aa2ef51
Fabian Thorand <f.thorand@gmail.com>
2020-08-04 22:49:49 +0200

    Build graphs more easily

--- examples/graphdemo.rs
+++ examples/graphdemo.rs
@@ -1,14 +1,55 @@
 //! Evaluating how the graph interface could be used
 
-use syn_txt::{wave::Stereo, graph::*};
+use syn_txt::{graph::*, wave::Stereo};
 
 fn main() {
-    let mut graph = Graph::new();
-    let n = graph.add_node(Box::new(Sine { samples_per_second: 44100, frequency: 440.0 }));
+    let mut builder = GraphBuilder::new();
+    let sine = builder
+        .add_node(Sine {
+            samples_per_second: 44100,
+            frequency: 440.0,
+        })
+        .build();
+    let sink = builder
+        .add_node(DebugSink)
+        .input_from(0, sine.output(0))
+        .build();
+
+    let mut graph = builder.build(1024);
     graph.step();
 }
 
+pub struct DebugSink;
 
+impl Node for DebugSink {
+    fn outputs(&self) -> &'static [&'static str] {
+        &[]
+    }
+
+    fn inputs(&self) -> &'static [&'static str] {
+        &["main"]
+    }
+
+    fn render(&mut self, rio: &RenderIo) {
+        let input = rio.input(0);
+        let half_width = 20;
+        let width = half_width * 2;
+        for s in input.samples() {
+            let lpos = half_width + (s.left * half_width as f64).round() as i64;
+            let rpos = half_width + (s.right * half_width as f64).round() as i64;
+
+            print!("|");
+            (0..lpos).for_each(|_| print!(" "));
+            print!("*");
+            (0..width - lpos).for_each(|_| print!(" "));
+            print!("|");
+            (0..rpos).for_each(|_| print!(" "));
+            print!("*");
+            (0..width - rpos).for_each(|_| print!(" "));
+            println!("|");
+        }
+    }
+}
 
 pub struct Sine {
     samples_per_second: Sample,
@@ -16,8 +57,12 @@ pub struct Sine {
 }
 
 impl Node for Sine {
-    fn num_outputs(&self) -> usize {
-        1
+    fn outputs(&self) -> &'static [&'static str] {
+        &["main"]
+    }
+
+    fn inputs(&self) -> &'static [&'static str] {
+        &[]
     }
 
     fn render(&mut self, rio: &RenderIo) {
--- src/graph.rs
+++ src/graph.rs
@@ -11,7 +11,99 @@ use crate::wave::AudioBuffer;
 /// Time measured in samples.
 pub type Sample = usize;
 
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
 pub struct NodeId(usize);
+
+impl NodeId {
+    pub fn input(self, index: usize) -> InputRef {
+        InputRef { node: self, index }
+    }
+
+    pub fn output(self, index: usize) -> OutputRef {
+        OutputRef { node: self, index }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
+pub struct InputRef {
+    node: NodeId,
+    index: usize,
+}
+
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
+pub struct OutputRef {
+    node: NodeId,
+    index: usize,
+}
+
+pub struct GraphBuilder {
+    nodes: Vec<Box<dyn Node>>,
+    edges: Vec<(OutputRef, InputRef)>,
+}
+
+impl GraphBuilder {
+    pub fn new() -> Self {
+        Self {
+            nodes: Vec::new(),
+            edges: Vec::new(),
+        }
+    }
+
+    pub fn add_node<'a, N: Node + 'static>(&'a mut self, node: N) -> NodeBuilder<'a> {
+        let id = NodeId(self.nodes.len());
+        self.nodes.push(Box::new(node));
+        NodeBuilder {
+            graph_builder: self,
+            node: id,
+        }
+    }
+
+    pub fn build(self, buffer_size: Sample) -> Graph {
+        let mut nodes: Vec<NodeHolder> = self
+            .nodes
+            .into_iter()
+            .map(|node| NodeHolder::new(node, buffer_size))
+            .collect();
+        // TODO: validate that there are no circles, otherwise, we might get runtime panics
+
+        // Connect the output buffers to the inputs
+        for (output, input) in self.edges {
+            nodes[input.node.0].input_buffers[input.index] =
+                Rc::clone(&nodes[output.node.0].output_buffers[output.index]);
+        }
+
+        Graph {
+            nodes,
+            time: 0,
+            buffer_size,
+        }
+    }
+}
+
+pub struct NodeBuilder<'a> {
+    graph_builder: &'a mut GraphBuilder,
+    node: NodeId,
+}
+
+impl<'a> NodeBuilder<'a> {
+    pub fn output_to(self, output_index: usize, input: InputRef) -> NodeBuilder<'a> {
+        self.graph_builder
+            .edges
+            .push((self.node.output(output_index), input));
+        self
+    }
+
+    pub fn input_from(self, input_index: usize, output: OutputRef) -> NodeBuilder<'a> {
+        self.graph_builder
+            .edges
+            .push((output, self.node.input(input_index)));
+        self
+    }
+
+    pub fn build(self) -> NodeId {
+        self.node
+    }
+}
 
 pub struct Graph {
     nodes: Vec<NodeHolder>,
@@ -28,19 +120,13 @@ impl Graph {
         }
     }
 
-    pub fn add_node(&mut self, node: Box<dyn Node>) -> NodeId {
-        let id = NodeId(self.nodes.len());
-        self.nodes.push(NodeHolder::new(node, self.buffer_size));
-        id
-    }
-
     pub fn step(&mut self) {
         for n in self.nodes.iter_mut() {
             let rio = RenderIo {
                 start: self.time,
                 length: self.buffer_size,
                 events: &[],
-                inputs: &[],
+                inputs: &n.input_buffers,
                 outputs: &n.output_buffers,
             };
             n.node.render(&rio);
@@ -49,26 +135,41 @@ impl Graph {
         self.time += self.buffer_size;
     }
 
+    // TODO: allow getting a reference back to a node and downcast?
+
     // pub fn connect(&mut self, from: NodeId, to: NodeId, )
 }
 
 struct NodeHolder {
     node: Box<dyn Node>,
+    input_buffers: Vec<Rc<RefCell<AudioBuffer>>>,
     output_buffers: Vec<Rc<RefCell<AudioBuffer>>>,
 }
 
 impl NodeHolder {
     fn new(node: Box<dyn Node>, buffer_size: Sample) -> Self {
-        let output_buffers = std::iter::repeat_with(|| Rc::new(RefCell::new(AudioBuffer::new(buffer_size)))).take(node.num_outputs()).collect();
+        // TODO: It is wasteful that we create input buffers here that are most likely
+        // immediately deallocated again when overwritten in the GraphBuilder
+        let input_buffers =
+            std::iter::repeat_with(|| Rc::new(RefCell::new(AudioBuffer::new(buffer_size))))
+                .take(node.inputs().len())
+                .collect();
+        let output_buffers =
+            std::iter::repeat_with(|| Rc::new(RefCell::new(AudioBuffer::new(buffer_size))))
+                .take(node.outputs().len())
+                .collect();
+
         Self {
             node,
+            input_buffers,
             output_buffers,
         }
     }
 }
 
 pub trait Node {
-    fn num_outputs(&self) -> usize;
+    fn inputs(&self) -> &'static [&'static str];
+    fn outputs(&self) -> &'static [&'static str];
     fn render(&mut self, rio: &RenderIo);
 }
 

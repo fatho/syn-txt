dd95fb32c06db49f95b8b65884b13bf18152bf59
Fabian Thorand <f.thorand@gmail.com>
2021-03-28 16:13:17 +0200

    Use monaco editor with yew

---
+++ syntxt-web-wasm/src/components/editor.rs
@@ -0,0 +1,108 @@
+use syntxt_lang::parser::NodePtr;
+use wasm_bindgen::prelude::*;
+use yew::{prelude::*, web_sys::HtmlElement};
+use yew::Properties;
+use serde::{Serialize,Deserialize};
+
+use crate::console_log;
+
+pub struct Editor {
+    link: ComponentLink<Self>,
+    container_ref: NodeRef,
+    on_content_changed: Callback<String>,
+    editor: Option<JsValue>,
+    change_handler: Closure<dyn Fn(String)>,
+}
+
+#[derive(Clone, PartialEq, Properties)]
+pub struct Props {
+    #[prop_or_default]
+    pub on_content_changed: Callback<String>,
+}
+
+pub enum Msg {
+    Test
+}
+
+impl Component for Editor {
+    type Message = Msg;
+    type Properties = Props;
+
+    fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        let on_content_changed = props.on_content_changed.clone();
+        Self {
+            link,
+            container_ref: NodeRef::default(),
+            on_content_changed: props.on_content_changed,
+            editor: None,
+            change_handler: Closure::wrap(Box::new(move |value| {
+                on_content_changed.emit(value);
+            })),
+        }
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        false
+    }
+
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        if self.on_content_changed != props.on_content_changed {
+            let on_content_changed = props.on_content_changed.clone();
+            self.on_content_changed = props.on_content_changed;
+
+            // Update handler
+            let new_change_handler = Closure::wrap(Box::new(move |value| {
+                on_content_changed.emit(value);
+            }) as Box<dyn Fn(String)>);
+
+            if let Some(editor) = self.editor.clone() {
+                onContentChanged(editor, &new_change_handler);
+            }
+
+            self.change_handler = new_change_handler;
+        }
+        // TODO: update callback
+        false
+    }
+
+    fn view(&self) -> Html {
+        //let callback = self.link.callback(|_| Msg::Test);
+        html! {
+            <div
+                ref=self.container_ref.clone()
+                style="height: 100%"
+                >
+            </div>
+        }
+    }
+
+    fn rendered(&mut self, first_render: bool) {
+        if first_render {
+            let container = self.container_ref.cast::<HtmlElement>().unwrap();
+            let editor = createEditor(&container);
+            onContentChanged(editor.clone(), &self.change_handler);
+            self.editor = Some(editor);
+        }
+    }
+
+    fn destroy(&mut self) {
+        if let Some(editor) = self.editor.take() {
+            destroyEditor(editor);
+        }
+    }
+}
+
+#[wasm_bindgen]
+extern "C" {
+    // #[wasm_bindgen(js_namespace = monaco.editor, js_name = create)]
+    // pub fn createEditor(container: &HtmlElement, props: JsValue);
+
+    #[wasm_bindgen(js_namespace = syntxt_helpers, js_name = createEditor)]
+    fn createEditor(container: &HtmlElement) -> JsValue;
+
+    #[wasm_bindgen(js_namespace = syntxt_helpers, js_name = onContentChanged)]
+    fn onContentChanged(editor: JsValue, callback: &Closure<dyn Fn(String)>);
+
+    #[wasm_bindgen(js_namespace = syntxt_helpers, js_name = destroyEditor)]
+    fn destroyEditor(editor: JsValue);
+}
--- syntxt-web-wasm/src/lib.rs
+++ syntxt-web-wasm/src/lib.rs
@@ -1,29 +1,16 @@
 use wasm_bindgen::prelude::*;
+use yew::prelude::*;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+pub mod components;
+pub mod console;
 
-    // Use `js_namespace` here to bind `console.log(..)` instead of just `log(..)`
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-}
-
-macro_rules! console_log {
-    // Note that this is using the `log` function imported above during
-    // `bare_bones`
-    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
-}
+use components::editor::Editor;
 
 #[wasm_bindgen(start)]
 pub fn run() {
     console_error_panic_hook::set_once();
+    App::<AppModel>::new().mount_to_body();
     console_log!("syntxt initialized");
-}
-
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, syntxt-web-wasm!");
 }
 
 #[wasm_bindgen]
@@ -39,5 +26,47 @@ pub fn parse(code: &str) -> Box<[JsValue]> {
             ])
         },
         Ok(_) => Box::new([]),
+    }
+}
+
+struct AppModel {
+    link: ComponentLink<Self>,
+    value: i64,
+}
+
+enum Msg {
+    SourceCodeChanged(String),
+}
+
+impl Component for AppModel {
+    type Message = Msg;
+    type Properties = ();
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self {
+            link,
+            value: 0,
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::SourceCodeChanged(code) => {
+                console_log!("Received {} characters", code.len());
+            }
+        }
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        // Should only return "true" if new properties are different to
+        // previously received properties.
+        // This component has no properties so we will always return "false".
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <Editor on_content_changed=self.link.callback(|code| Msg::SourceCodeChanged(code))/>
+        }
     }
 }
